231d229380d1e51fea391611dfa8ae8c2560872d
Edilene <edilene_cruz@yahoo.com.br>
2017-04-18 17:45:26 -0600

    full CRUD for trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,13 +5,14 @@ class TripsController < ApplicationController
   end
 
   def create
-    @user = current_user
-    @trip = @user.trips.create(trip_params)
+    user = current_user
+    @trip = Trip.create(trip_params)
     if @trip.save
-      flash[:success] = "Your trip has been created!"
+      user.trips << @trip
+      flash.now[:success] = "Your trip has been created!"
       redirect_to trip_path(@trip)
     else
-      flash[:error] = @trip.errors.full_messages.to_sentence
+      flash.now[:error] = @trip.errors.full_messages.to_sentence
       render :new
     end
   end
@@ -19,6 +20,28 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
     # @prices = Price.get_prices(@trip.city)
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    if @trip.update(trip_params)
+      flash[:success] = "Your trip has been updated."
+      redirect_to trip_path(@trip)
+    else
+      flash[:danger] = "Your trip was not updated."
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    redirect_to "/#{@user.slug}"
   end
 
   private
---
+++ spec/features/trips/user_can_delete_trip_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "delete trip" do
+  before(:each) do
+    @user = Fabricate(:user)
+    @trips = Fabricate.times(2, :trip)
+    @user.trips << @trips
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
+  it "can delete a trip" do
+
+    visit "/#{@user.slug}"
+
+    within(".trips-table") do
+      first(:link, "Delete").click 
+    end
+
+    expect(@user.trips.count).to eq(1)
+  end
+
+end
