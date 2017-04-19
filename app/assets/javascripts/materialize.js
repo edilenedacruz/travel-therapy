3021fc391bd51686230a857fa16df1230184a659
Edilene <edilene_cruz@yahoo.com.br>
2017-04-18 18:57:31 -0600

    flash working but without color

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -12,4 +12,18 @@ $(document).ready(function() {
 $(document).ready(function(){
   $('.slider').slider();
 });
-    
+
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+};
