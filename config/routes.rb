b1d2f4951faadc29c0feacce1c173293f871bd13
Edilene <edilene_cruz@yahoo.com.br>
2017-04-23 18:03:23 -0600

    maybe removing regular user create will make this work

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get '/auth/google_oauth2/callback', to: 'sessions#create_facebook'
-  # post '/login', to: 'sessions#create'
   get '/auth/google_oauth2',          as: :login
 #
-  # get '/login', to: 'sessions#new', as: "login"
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
 
   resources :users
-
+  
   resources :trips do
     resources :todos
   end
